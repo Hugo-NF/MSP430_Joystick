cad2d6c88758833f2ead2c94c479923c2cb5563b
José Luiz <joseluizgnogueira@live.com>
2018-06-04 15:46:02 -0300

    Joystick

---
+++ Modulo3/Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+main.obj: ../pins.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdint.h
+main.obj: ../msp_timers.h
+
+../main.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
+../pins.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdint.h: 
+../msp_timers.h: 
